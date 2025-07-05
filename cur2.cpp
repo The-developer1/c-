3ac89cb2a0ce28737465bc37eeff9e8476882e68
The-developer1 <newiphon055@gmail.com>
2025-07-06 01:11:38 +0300

    Create cur2.cpp

---
+++ cur2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// all code will write her
+	
+	int age=24;
+
+	double pi=3.4;
+
+	char type='a';
+
+
+	int oldage = 15;
+	age = oldage;
+
+
+	cout << "age:" << age << ",pi:" << pi << ",type:" << type<<"\n";
+
+	system("pause");// لاجل تثبت شاشة التنفيذ السوداء ولا تختغي
+
+	return 0;
+}
+// 4 video 
