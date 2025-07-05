deeea91252bddafe89d718c5666bf3d58ba47ce7
The-developer1 <newiphon055@gmail.com>
2025-07-06 00:35:38 +0300

    Create cur1.cpp

---
+++ cur1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	// all code will write her
+
+
+	cout << "hello my followers\n";
+
+	system("pause");// لاجل تثبت شاشة التنفيذ السوداء ولا تختغي
+
+	return 0;
+}
